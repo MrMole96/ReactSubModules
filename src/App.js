819b5bd159ec2d55ddc08d9363c0d94be781eff4
Wojtek Rupik <w.rupik@selleo.com>
2021-09-01 10:14:33 +0200

    another commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { sharedFunction } from "./ReduxSubModules/src/logic/counter";
+import "./App.css";
 
 function App() {
+  const onClickHandler = () => {
+    sharedFunction('WEB');
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +21,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => onClickHandler()}>test</button>
       </header>
     </div>
   );
